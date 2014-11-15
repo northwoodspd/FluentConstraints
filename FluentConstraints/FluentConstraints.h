55b993c246556afefdf0dd5c199a227641fc44bd
Steve Madsen <steve.madsen@teamnorthwoods.com>
2014-11-15 12:18:29 -0500

    Initial Commit

---
+++ FluentConstraints/FluentConstraints.h
@@ -0,0 +1,19 @@
+//
+//  FluentConstraints.h
+//  FluentConstraints
+//
+//  Created by Steve Madsen on 11/15/14.
+//  Copyright (c) 2014 Light Year Software, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FluentConstraints.
+FOUNDATION_EXPORT double FluentConstraintsVersionNumber;
+
+//! Project version string for FluentConstraints.
+FOUNDATION_EXPORT const unsigned char FluentConstraintsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FluentConstraints/PublicHeader.h>
+
+
---
+++ FluentConstraintsTests/FluentConstraintsTests.swift
@@ -0,0 +1,36 @@
+//
+//  FluentConstraintsTests.swift
+//  FluentConstraintsTests
+//
+//  Created by Steve Madsen on 11/15/14.
+//  Copyright (c) 2014 Light Year Software, LLC. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class FluentConstraintsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
