28d04eab6364b1df80162616557a1f28226f8886
Steve Madsen <steve.madsen@teamnorthwoods.com>
2015-06-26 11:08:33 -0400

    Implemented about half of the remaining attributes

--- FluentConstraints/FluentConstraints.swift
+++ FluentConstraints/FluentConstraints.swift
@@ -54,4 +54,94 @@ public class FluentConstraint {
         self.firstAttribute = .Width
         return self
     }
+
+    public var baseline: FluentConstraint {
+        if secondItem == nil {
+            self.firstAttribute = .Baseline
+        } else {
+            self.secondAttribute = .Baseline
+        }
+        return self
+    }
+
+    public var firstBaseline: FluentConstraint {
+        if secondItem == nil {
+            self.firstAttribute = .FirstBaseline
+        } else {
+            self.secondAttribute = .FirstBaseline
+        }
+        return self
+    }
+
+    public var leftMargin: FluentConstraint {
+        if secondItem == nil {
+            self.firstAttribute = .LeftMargin
+        } else {
+            self.secondAttribute = .LeftMargin
+        }
+        return self
+    }
+
+    public var rightMargin: FluentConstraint {
+        if secondItem == nil {
+            self.firstAttribute = .RightMargin
+        } else {
+            self.secondAttribute = .RightMargin
+        }
+        return self
+    }
+
+    public var topMargin: FluentConstraint {
+        if secondItem == nil {
+            self.firstAttribute = .TopMargin
+        } else {
+            self.secondAttribute = .TopMargin
+        }
+        return self
+    }
+
+    public var bottomMargin: FluentConstraint {
+        if secondItem == nil {
+            self.firstAttribute = .BottomMargin
+        } else {
+            self.secondAttribute = .BottomMargin
+        }
+        return self
+    }
+
+    public var leadingMargin: FluentConstraint {
+        if secondItem == nil {
+            self.firstAttribute = .LeadingMargin
+        } else {
+            self.secondAttribute = .LeadingMargin
+        }
+        return self
+    }
+
+    public var trailingMargin: FluentConstraint {
+        if secondItem == nil {
+            self.firstAttribute = .TrailingMargin
+        } else {
+            self.secondAttribute = .TrailingMargin
+        }
+        return self
+    }
+
+    public var centerXWithinMargins: FluentConstraint {
+        if secondItem == nil {
+            self.firstAttribute = .CenterXWithinMargins
+        } else {
+            self.secondAttribute = .CenterXWithinMargins
+        }
+        return self
+    }
+
+    public var centerYWithinMargins: FluentConstraint {
+        if secondItem == nil {
+            self.firstAttribute = .CenterYWithinMargins
+        } else {
+            self.secondAttribute = .CenterYWithinMargins
+        }
+        return self
+    }
 }
--- FluentConstraintsTests/FluentConstraintsTests.swift
+++ FluentConstraintsTests/FluentConstraintsTests.swift
@@ -37,6 +37,66 @@ class FluentConstraintsTests: XCTestCase {
         expect(constraint.secondAttribute) == NSLayoutAttribute.Top
     }
 
+    func testBaselineAttribute() {
+        let constraint = FluentConstraint(firstView).baseline.equalTo(secondView).baseline.build()
+        expect(constraint.firstAttribute) == NSLayoutAttribute.Baseline
+        expect(constraint.secondAttribute) == NSLayoutAttribute.Baseline
+    }
+
+    func testFirstBaselineAttribute() {
+        let constraint = FluentConstraint(firstView).firstBaseline.equalTo(secondView).firstBaseline.build()
+        expect(constraint.firstAttribute) == NSLayoutAttribute.FirstBaseline
+        expect(constraint.secondAttribute) == NSLayoutAttribute.FirstBaseline
+    }
+
+    func testLeftMarginAttribute() {
+        let constraint = FluentConstraint(firstView).leftMargin.equalTo(secondView).leftMargin.build()
+        expect(constraint.firstAttribute) == NSLayoutAttribute.LeftMargin
+        expect(constraint.secondAttribute) == NSLayoutAttribute.LeftMargin
+    }
+
+    func testRightMarginAttribute() {
+        let constraint = FluentConstraint(firstView).rightMargin.equalTo(secondView).rightMargin.build()
+        expect(constraint.firstAttribute) == NSLayoutAttribute.RightMargin
+        expect(constraint.secondAttribute) == NSLayoutAttribute.RightMargin
+    }
+
+    func testTopMarginAttribute() {
+        let constraint = FluentConstraint(firstView).topMargin.equalTo(secondView).topMargin.build()
+        expect(constraint.firstAttribute) == NSLayoutAttribute.TopMargin
+        expect(constraint.secondAttribute) == NSLayoutAttribute.TopMargin
+    }
+
+    func testBottomMarginAttribute() {
+        let constraint = FluentConstraint(firstView).bottomMargin.equalTo(secondView).bottomMargin.build()
+        expect(constraint.firstAttribute) == NSLayoutAttribute.BottomMargin
+        expect(constraint.secondAttribute) == NSLayoutAttribute.BottomMargin
+    }
+
+    func testLeadingMarginAttribute() {
+        let constraint = FluentConstraint(firstView).leadingMargin.equalTo(secondView).leadingMargin.build()
+        expect(constraint.firstAttribute) == NSLayoutAttribute.LeadingMargin
+        expect(constraint.secondAttribute) == NSLayoutAttribute.LeadingMargin
+    }
+
+    func testTrailingMarginAttribute() {
+        let constraint = FluentConstraint(firstView).trailingMargin.equalTo(secondView).trailingMargin.build()
+        expect(constraint.firstAttribute) == NSLayoutAttribute.TrailingMargin
+        expect(constraint.secondAttribute) == NSLayoutAttribute.TrailingMargin
+    }
+
+    func testCenterXWithinMarginsAttribute() {
+        let constraint = FluentConstraint(firstView).centerXWithinMargins.equalTo(secondView).centerXWithinMargins.build()
+        expect(constraint.firstAttribute) == NSLayoutAttribute.CenterXWithinMargins
+        expect(constraint.secondAttribute) == NSLayoutAttribute.CenterXWithinMargins
+    }
+
+    func testCenterYWithinMarginsAttribute() {
+        let constraint = FluentConstraint(firstView).centerYWithinMargins.equalTo(secondView).centerYWithinMargins.build()
+        expect(constraint.firstAttribute) == NSLayoutAttribute.CenterYWithinMargins
+        expect(constraint.secondAttribute) == NSLayoutAttribute.CenterYWithinMargins
+    }
+
     // MARK: relation tests
 
     func testEqualToRelation() {
@@ -44,13 +104,13 @@ class FluentConstraintsTests: XCTestCase {
         expect(constraint.relation) == NSLayoutRelation.Equal
     }
 
-    func testEqualToArgumentIsConstraintSecondItem() {
+    func testEqualToArgumentIsView() {
         let constraint = FluentConstraint(firstView).top.equalTo(secondView).top.build()
         expect(constraint.secondItem).notTo(beNil())
         expect(constraint.secondItem as? UIView) == secondView
     }
 
-    func testEqualToWithNumberAssignsConstant() {
+    func testEqualToArgumentIsConstant() {
         let constraint = FluentConstraint(firstView).width.equalTo(10).build()
         expect(constraint.constant) == CGFloat(10.0)
     }
